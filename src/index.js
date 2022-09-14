a99dc1df07886a7ba2ad23cbd5c34540d532a8d8
thanhdong123456 <dongcoi.tk@gmail.com>
2022-09-14 15:43:25 +0700

    init source

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/thanhdong123456.github.io">
     <App />
   </BrowserRouter>
 );
